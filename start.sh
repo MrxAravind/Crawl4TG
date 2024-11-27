84e44d2e9f7d1877a306ba44e5bf68433a371fc4
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-11-27 22:06:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+python -m venv myenv  # Create a virtual environment (if not already created)
+source myenv/bin/activate  # Activate the virtual environment
+python -m ensurepip --upgrade  # Ensure pip is installed and upgrade it
 pip install -r requirements.txt
 python -m playwright install chromium
 python main.py
