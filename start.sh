99bd8e44ad12b578562d0cac503d950696dd7eaf
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-12-01 07:52:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-python3 -m venv myenv  # Create a virtual environment (if not already created)
-source myenv/bin/activate  # Activate the virtual environment
-python3 -m ensurepip --upgrade  # Ensure pip is installed and upgrade it
 pip install -r requirements.txt
-python3 -m playwright install chromium
-python3 app.py
+crawl4ai-setup 
+python3 run.py
